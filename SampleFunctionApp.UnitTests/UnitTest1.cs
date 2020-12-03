0e0a4c7d94aea27940393c8a1bc5e3fe213e2bac
PhilOldale <Philip.Oldale@landmark.co.uk>
2020-12-03 12:04:43 +0000

    initial commit

---
+++ SampleFunctionApp.UnitTests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SampleFunctionApp.UnitTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
